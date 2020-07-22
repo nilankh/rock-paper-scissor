e21bd6449d4cae32e92d9469ea40a9853774284c
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-22 21:59:55 +0530

    working on javascript part

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 const game = () => {
-    
-}
+  let pScore = 0;
+  let cScore = 0;
+
+  const startGame = () => {
+    const playBtn = document.querySelector(".intro button");
+    const introScreen = document.querySelector(".intro");
+    const match = document.querySelector(".match");
+
+    playBtn.addEventListener("click", () => {
+      introScreen.classList.add("fadeOut");
+    });
+  };
+  //   Is call all the inner function
+  startGame();
+  updateScore();
+};
+
+// start the game fucntion
+game();
