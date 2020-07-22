5a7d8f44e19cdc56ce0c7dd6e65d38d5db69ed56
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-22 22:26:12 +0530

    started working on javascript part

--- app.js
+++ app.js
@@ -13,17 +13,29 @@ const game = () => {
       match.classList.add("fadeIn");
     });
   };
-//   Play Match
-const playMatch = () => {
-    const options = document.querySelectorAll('options button');
-    const playerHand = document.querySelector('.player-hand');
-    const computerHand = document.querySelectorAll('.computer-hand');
-}
+  //   Play Match
+  const playMatch = () => {
+    const options = document.querySelectorAll("options button");
+    const playerHand = document.querySelector(".player-hand");
+    const computerHand = document.querySelectorAll(".computer-hand");
+    // computer options
+    const computerOptions = ["rock", "paper", "scissors"];
+
+    options.forEach((option) => {
+      option.addEventListener("click", function(){
+        // computer choice  
+        const computerNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[computerNumber]
+        console.log(computerChoice);
+    });
+    });
+  };
 
   //   Is call all the inner function
   startGame();
-  updateScore();
+  playMatch();
 };
 
 // start the game fucntion
 game();
+
